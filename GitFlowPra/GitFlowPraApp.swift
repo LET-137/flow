548a0e45dd4dceffb3571629caa61501dd6ed4e0
LET <tsumototakuya@LETM3.local>
2024-11-20 22:28:55 +0900

    first commit

---
+++ GitFlowPra/GitFlowPraApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFlowPraApp.swift
+//  GitFlowPra
+//
+//  Created by 津本拓也 on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct GitFlowPraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
